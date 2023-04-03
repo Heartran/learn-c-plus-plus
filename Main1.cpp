49be58cfb9b3e0843b18c9cc26c3a55442ee3254
Heartran <heartrangl@gmail.com>
2023-04-03 22:47:29 +0200

    Learning variables

---
+++ Main1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello World!" ;
+    int etaDiNicolo;
+    etaDiNicolo = 30 + 1;
+    float altezzaDiNicolo;
+    altezzaDiNicolo = 1.97;
+    char nomeDiNicolo[8];
+    nomeDiNicolo = "Nicolò";
+    char cognomeDiNicolo[5];
+    cognomeDiNicolo = "Rogai";
+    int numeriPreferitiDiNicolo[5];
+    numeriPreferitiDiNicolo[0] = 25;
+}
