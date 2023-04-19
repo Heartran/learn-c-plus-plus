5ec07b47fd91ca5739c13897f61926f8d97f3ad9
Heartran <heartrangl@gmail.com>
2023-04-19 19:14:42 +0200

    Variables achieved

--- Main1.cpp
+++ Main1.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
 int main() {
-    cout << "Hello World!" ;
+    cout << "Hello World!" << endl;
     int etaDiNicolo;
     etaDiNicolo = 30 + 1;
     float altezzaDiNicolo;
     altezzaDiNicolo = 1.97;
     char nomeDiNicolo[8];
-    nomeDiNicolo = "Nicolò";
+    strcpy(nomeDiNicolo, "Nicolò");
     char cognomeDiNicolo[5];
-    cognomeDiNicolo = "Rogai";
+    strcpy(cognomeDiNicolo, "Rogai");
     int numeriPreferitiDiNicolo[5];
     numeriPreferitiDiNicolo[0] = 25;
+    long etaDiNicoloInSecondi;
+    etaDiNicoloInSecondi = etaDiNicolo * 12 * 30 * 24 * 60 * 60;
+    cout << etaDiNicoloInSecondi;
 }
